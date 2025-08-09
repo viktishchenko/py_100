c33dc006fd052728b405ae0120bf5cf5b800a5e8
Viktor Tishchenko <9043258451vvt@gmail.com>
2025-08-09 15:15:08 +0600

    feat: fix.gen.pass

--- int/day29/main.py
+++ int/day29/main.py
@@ -11,6 +11,7 @@ FONT_INPUT = ('New Roman', 12, 'normal')
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 def generate_password():
+  password = ''
   letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
   numbers = ['0','1','2','3','4','5','6','7','8','9']
   symbols = ['!','#','$','%','&','(', ')','*','+']
@@ -42,6 +43,7 @@ def generate_password():
 
   # OR_______________________________________________________________
   password = ''.join(res)
+  pass_input.delete(0,END)
   pass_input.insert(0, password)
   pyperclip.copy(password)
 # ---------------------------- SAVE PASSWORD ------------------------------- #

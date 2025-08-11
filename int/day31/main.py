2e3883048bb994396a639f835e088420c342df09
Viktor Tishchenko <9043258451vvt@gmail.com>
2025-08-11 07:46:14 +0600

    feat: flip.timer.lgc

--- int/day31/main.py
+++ int/day31/main.py
@@ -17,11 +17,21 @@ data = pandas.read_csv('int/day31/data/data_ru.csv')
 # card_data = data.to_dict() # {'English': {0: 'the', ... 1046: 'Paul'}, 'Russian': {0: 'то', ... 1046: 'Пол'}}
 card_data = data.to_dict(orient='records') # [{'English': 'the', 'Russian': 'то'}, ...  {'English': 'Paul', 'Russian': 'Пол'}]
 
+current_card ={}
 
 def next_card():
+  global current_card, flip_timer
+  window.after_cancel(flip_timer)
   current_card = random.choice(card_data)
-  canvas.itemconfig(card_title, text='English')
-  canvas.itemconfig(card_word, text=current_card['English'])
+  canvas.itemconfig(card_title, text='English', fill='black')
+  canvas.itemconfig(card_word, text=current_card['English'], fill='black')
+  canvas.itemconfig(card_background, image=card_front_img)
+  flip_timer = window.after(3000,func=flip_card)
+
+def flip_card():
+  canvas.itemconfig(card_title, text='Русский', fill='white')
+  canvas.itemconfig(card_word, text=current_card['Russian'], fill='white')
+  canvas.itemconfig(card_background, image=card_back_img)
 
 
 window = Tk()
@@ -29,9 +39,12 @@ window.minsize(450, 200)
 window.title('Flash Card')
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+flip_timer = window.after(3000, func=flip_card)
+
 canvas = Canvas(width=800, height=526)
 card_front_img = PhotoImage(file='int/day31/images/card_front.png')
-canvas.create_image(400, 263, image=card_front_img)
+card_back_img = PhotoImage(file='int/day31/images/card_back.png')
+card_background = canvas.create_image(400, 263, image=card_front_img)
 card_title = canvas.create_text(400, 150, text='', font=('Ariel', 40, 'italic') )
 card_word = canvas.create_text(400, 263, text='', font=('Ariel', 60, 'bold') )
 
